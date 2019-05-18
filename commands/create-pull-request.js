ef89e24cc85bd6b580ae4636787b1ba6dd504f50
Sebastian Richter <s.richter@rmh-media.com>
2019-05-18 14:51:58 +0200

    automatically fetching commit-messages

--- commands/create-pull-request.js
+++ commands/create-pull-request.js
@@ -3,9 +3,29 @@ const ora = require('ora')
 const prettyjson = require('prettyjson')
 
 module.exports = ({ program }) => {
+  let client = null
+
   const reviewers = []
+
   const addReviewer = (value) => {
     reviewers.push(value)
+  }
+
+  const fetchDescription = function (source, target) {
+    return client.get(`/commits/${source}`, {
+      params: {
+        exclude: target
+      }
+    })
+      .then((r) => {
+        const messages = r.data.values.map(commit => '* ' + commit.rendered.message.raw).join("")
+
+        return [
+          '**Included Messages (fetched)**',
+          messages
+        ].join('\n\n')
+      })
+      .catch((e) => 'No fetchable Data')
   }
 
   program
@@ -14,12 +34,28 @@ module.exports = ({ program }) => {
     .option('-s, --source <source>', 'Source Branch', 'develop')
     .option('-t, --target <target>', 'Target Branch', 'master')
     .option('-r, --reviewer <reviewer>', 'Add one or more reviewers by username (only Username works), use once with comma-separated values or multiple times', addReviewer)
-    .option('-d, --description <description>', 'Describe the PR, supports Markdown')
+    .option('-d, --description <description>', 'Describe the PR, supports Markdown or leave it to us to generate a comprehensive description')
     .option('-u, --username <username>', 'Username to connect to bitbucket')
     .option('-p, --password <password>', 'Password to connect to bitbucket')
     .option('--keep-branch', 'Should BB keep the branch open after merge?')
+    .option('--no-fail', 'Command will not exit with code != 0 even on failure')
+    .option('--debug', 'Output message to be sent to Bitbucket API')
     .action((repoSlug, title, cmd) => {
       const spinner = ora(`Creating Pull Request at ${repoSlug}`).start()
+
+      client = axios.create({
+        baseURL: `https://api.bitbucket.org/2.0/repositories/${repoSlug}`,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true,
+        auth: {
+          username: cmd.username,
+          password: cmd.password
+        },
+        responseType: 'json'
+      })
 
       const message = {
         title: title,
@@ -36,11 +72,6 @@ module.exports = ({ program }) => {
         }
       }
 
-      // default = function due to commander
-      if (typeof cmd.description === 'string') {
-        message.description = cmd.description
-      }
-
       let mappableReviewers = reviewers
 
       if (reviewers.length === 1 && !!reviewers[0]) {
@@ -55,29 +86,37 @@ module.exports = ({ program }) => {
         })
       }
 
-      const url = `https://api.bitbucket.org/2.0/repositories/${repoSlug}/pullrequests`
+      const findDescription = typeof cmd.description === 'string'
+        ? Promise.resolve(cmd.description)
+        : fetchDescription(message.source.branch.name, message.target.branch.name)
 
-      axios.post(url, message, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        withCredentials: true,
-        auth: {
-          username: cmd.username,
-          password: cmd.password
-        },
-        responseType: 'json'
-      }).then((r) => {
-        spinner.succeed('Successfully created PR ' + r.data.links.html.href)
+      findDescription
+        .then(description => {
+          message.description = description
 
-        process.exit()
-      }).catch((e) => {
-        spinner.fail('Failed creating PR with Status ' + e.response.status)
-        console.log(prettyjson.render(e.response.data, { keysColor: 'red' }))
+          return message
+        })
+        .then(message => {
+          if (cmd.debug) {
+            console.log('\n', prettyjson.render(message))
+          }
+          return message
+        })
+        .then(data => client.post('/pullrequests', data))
+        .then((r) => {
+          spinner.succeed('Successfully created PR ' + r.data.links.html.href)
 
-        process.exit(1) // fail
-      })
+          process.exit()
+        })
+        .catch((e) => {
+          console.log(e)
+          spinner.fail('Failed creating PR with Status ' + e.response.status)
+          console.log(prettyjson.render(e.response.data, { keysColor: 'red' }))
+
+          if (cmd.fail) {
+            process.exit(1)
+          }
+        })
     })
 }
 
